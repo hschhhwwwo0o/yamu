fe6b73790ba8c1369190ec3799a1c02526fdbce9
Saveliy Andronov <ssandry.github@gmail.com>
2023-07-18 17:13:07 +0300

    Add select device test

--- app/yamu-react-app/app/page.tsx
+++ app/yamu-react-app/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 import { MockUpGenerator } from "@module/mock-up-generator";
 import { MockUpHTMLRenderer } from "@module/mock-up-html-renderer";
@@ -9,47 +9,60 @@ export default function IndexScreen() {
   const mockUpGenerator = new MockUpGenerator();
   const mockUpHTMLRenderer = new MockUpHTMLRenderer("mock-up-container");
 
-  useEffect(function () {
-    mockUpGenerator.selectDevice("iWatch SE");
-    mockUpGenerator.mockUp.device.changeSettings({
-      isStrap: true,
-    });
-  }, []);
+  const [selectedDeviceName, setSelectedDeviceName] =
+    useState<string>("iWatch SE");
 
-  useEffect(function () {
-    mockUpHTMLRenderer.render({
-      frameWidth: mockUpGenerator.mockUp.device.width,
-      frameHeight: mockUpGenerator.mockUp.device.height,
-      frameImage: mockUpGenerator.mockUp.device.frameImage,
-      insertedImage: mockUpGenerator.mockUp.insertedImage,
-      paddingsInPercents: {
-        paddingTop:
-          mockUpGenerator.mockUp.device.deviceLibraryItem.paddingsInPercents
-            .paddingTop,
-        paddingLeft:
-          mockUpGenerator.mockUp.device.deviceLibraryItem.paddingsInPercents
-            .paddingLeft,
-        paddingBottom:
-          mockUpGenerator.mockUp.device.deviceLibraryItem.paddingsInPercents
-            .paddingBottom,
-        paddingRight:
-          mockUpGenerator.mockUp.device.deviceLibraryItem.paddingsInPercents
-            .paddingRight,
-      },
-    });
-  }, []);
+  useEffect(
+    function () {
+      mockUpGenerator.selectDevice(selectedDeviceName);
+      mockUpHTMLRenderer.render({
+        frameWidth: mockUpGenerator.mockUp.device.width,
+        frameHeight: mockUpGenerator.mockUp.device.height,
+        frameImage: mockUpGenerator.mockUp.device.frameImage,
+        insertedImage: mockUpGenerator.mockUp.insertedImage,
+        paddingsInPercents: {
+          paddingTop:
+            mockUpGenerator.mockUp.device.deviceLibraryItem.paddingsInPercents
+              .paddingTop,
+          paddingLeft:
+            mockUpGenerator.mockUp.device.deviceLibraryItem.paddingsInPercents
+              .paddingLeft,
+          paddingBottom:
+            mockUpGenerator.mockUp.device.deviceLibraryItem.paddingsInPercents
+              .paddingBottom,
+          paddingRight:
+            mockUpGenerator.mockUp.device.deviceLibraryItem.paddingsInPercents
+              .paddingRight,
+        },
+      });
+    },
+    [selectedDeviceName],
+  );
+
+  async function downloadMockUp(): Promise<void> {
+    await mockUpHTMLRenderer.download("png");
+  }
+
+  function selectDevice(event: React.ChangeEvent<HTMLSelectElement>): void {
+    setSelectedDeviceName(event.target.value);
+  }
 
   return (
     <div className="w-[100vw] flex flex-col items-center justify-center pt-10">
+      <div className="py-2">
+        <select
+          onChange={selectDevice}
+          name="select-device-mock-up"
+          id="select-device-mock-up"
+        >
+          <option value="iWatch SE">iWatch SE</option>
+          <option value="iPhone 13">iPhone 13</option>
+          <option value="iPhone 14 Pro">iPhone 14 Pro</option>
+        </select>
+      </div>
       <div id="mock-up-container" />
       <div className="w-[100vw] flex items-center justify-center py-6">
-        <button
-          onClick={() => {
-            mockUpHTMLRenderer.download("png");
-          }}
-        >
-          Download mock-up
-        </button>
+        <button onClick={downloadMockUp}>Download mock-up</button>
       </div>
     </div>
   );

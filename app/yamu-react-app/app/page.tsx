26ecd5d16cf1050d107e226666b61f31c8954292
Saveliy Andronov <ssandry.github@gmail.com>
2023-08-17 16:58:28 +0300

    Main div

--- app/yamu-react-app/app/page.tsx
+++ app/yamu-react-app/app/page.tsx
@@ -3,7 +3,5 @@
 import React from "react";
 
 export default function IndexScreen() {
-  return (
-    <div className="w-[100vw] flex flex-col items-center justify-center pt-10"></div>
-  );
+  return <main className=""></main>;
 }

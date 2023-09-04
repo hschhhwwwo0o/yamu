526cf50e026b25990163fbd1eb393b982f871b10
Saveliy Andronov <ssandry.github@gmail.com>
2023-09-04 17:13:31 +0300

    fix husky

--- scripts/project_install.bash
+++ scripts/project_install.bash
@@ -1,2 +1,3 @@
 npx husky-init && npm install
+
 cd ./app/yamu-react-app && npm install && cd ../../ && cd ./modules/mock-up-html-renderer && npm install && cd ../../ && cd ./modules/mock-up-generator && npm install && cd ../../ && npm install && npm run re-build-modules

b434b94bb7b9dc54332ea52b411ddba35d2d6fa8
Saveliy Andronov <ssandry.github@gmail.com>
2023-09-04 17:03:47 +0300

    Fix scripts

--- scripts/build_modules.bash
+++ scripts/build_modules.bash
@@ -1,5 +1,44 @@
 #!/bin/bash 
 
-cd ./modules/mock-up-generator && npm run compile
-cd ../../
-cd ./modules/mock-up-html-renderer && npm run compile
+case $1 in
+  "--all") 
+    cd ./modules/mock-up-generator && npm run compile
+    cd ../../
+    cd ./modules/mock-up-html-renderer && npm run compile
+    cd ../../
+    echo "All modules have been successfully compiled ✅ 🌿"
+    exit 0
+  ;;
+esac
+
+echo ""
+echo "Select the module to compile: "
+echo "  1) All"
+echo "  2) @mock-up-generator"
+echo "  3) @mock-up-html-renderer"
+echo "  4) Quit"
+
+read n
+case $n in
+  1) 
+    cd ./modules/mock-up-generator && npm run compile
+    cd ../../
+    cd ./modules/mock-up-html-renderer && npm run compile
+    cd ../../
+    echo "All modules have been successfully compiled ✅ 🌿"
+  ;;
+  2) 
+    cd ./modules/mock-up-generator && npm run compile
+    cd ../../
+    echo "@mock-up-generator module successfully compiled ✅ 🌿"
+  ;;
+  3) 
+    cd ./modules/mock-up-html-renderer && npm run compile
+    cd ../../
+    echo "@mock-up-html-renderer module successfully compiled ✅ 🌿"
+  ;;
+  4) 
+    exit 0
+  ;;
+  *) echo "Invalid option";;
+esac
--- scripts/lint_modules.bash
+++ scripts/lint_modules.bash
@@ -3,3 +3,7 @@
 cd ./modules/mock-up-generator && npm run lint
 cd ../../
 cd ./modules/mock-up-html-renderer && npm run lint
+cd ../../
+
+echo ""
+echo "All modules have been successfully tested ✅ 🌿"

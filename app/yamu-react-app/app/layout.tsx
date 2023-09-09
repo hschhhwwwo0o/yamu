fd9e4fbd254c9b6ea17148070064f8481fd6ae19
Saveliy Andronov <ssandry.github@gmail.com>
2023-09-09 09:22:20 +0300

    OG metadata

--- app/yamu-react-app/app/layout.tsx
+++ app/yamu-react-app/app/layout.tsx
@@ -26,6 +26,7 @@ export const metadata: Metadata = {
     description:
       "Create product mock-ups with the online mock-up generator. Simply select a mock-up, upload your design and download a watermark-free image.",
     type: "website",
+    images: ["/og/OpenGraphCover.png"],
   },
 };
 

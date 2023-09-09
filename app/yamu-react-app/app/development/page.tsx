0c65e4511b9c7820ee98ae39563cb5cd7bdad59e
Saveliy Andronov <ssandry.github@gmail.com>
2023-09-09 08:02:40 +0300

    Initialize pages structure

---
+++ app/yamu-react-app/app/development/page.tsx
@@ -0,0 +1,9 @@
+import React, { Fragment } from "react";
+
+export default function DevelopmentScreen(): React.JSX.Element {
+  return (
+    <main>
+      <Fragment />
+    </main>
+  );
+}
---
+++ app/yamu-react-app/app/license/page.tsx
@@ -0,0 +1,9 @@
+import React, { Fragment } from "react";
+
+export default function LicenseScreen(): React.JSX.Element {
+  return (
+    <main>
+      <Fragment />
+    </main>
+  );
+}

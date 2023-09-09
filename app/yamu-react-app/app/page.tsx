91426f108edfd5382b2497bf9ae7ed73e7d868cd
Saveliy Andronov <ssandry.github@gmail.com>
2023-09-09 09:39:58 +0300

    Add link component

--- app/yamu-react-app/app/page.tsx
+++ app/yamu-react-app/app/page.tsx
@@ -1,8 +1,10 @@
+import { LinkCustom } from "@/components/text/Link";
 import React, { Fragment } from "react";
 
 export default function IndexScreen(): React.JSX.Element {
   return (
     <main>
+      <LinkCustom href="/">Hi</LinkCustom>
       <Fragment />
     </main>
   );
--- app/yamu-react-app/tailwind.config.js
+++ app/yamu-react-app/tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "brand-blue": "#007AFF",
+      },
+    },
   },
   plugins: [],
 };

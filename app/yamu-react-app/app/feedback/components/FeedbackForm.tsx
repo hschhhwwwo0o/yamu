7b8dc36b5ecd48670a10a57145cb4ea385d5c13b
Saveliy Andronov <ssandry.github@gmail.com>
2023-09-10 14:54:10 +0300

    Some fixs

--- app/yamu-react-app/app/feedback/components/FeedbackForm.tsx
+++ app/yamu-react-app/app/feedback/components/FeedbackForm.tsx
@@ -7,13 +7,11 @@ import { Textarea, useTextarea } from "@/components/form/Textarea";
 import { Button, useButton } from "@/components/form/Button";
 
 export function FeedbackForm() {
-  const feedbackTextarea = useTextarea({
-    title: "Enter feedback",
-  });
+  const feedbackTextarea = useTextarea({});
 
   const createFeedbackButton = useButton({
     loadingText: "Creating feedback...",
-    isDisabled: feedbackTextarea.props.value.length < 1,
+    isDisabled: feedbackTextarea.props.value.length < 5,
     onClick() {
       createFeedbackButton.utils.startLoading();
       setTimeout(() => {
@@ -26,12 +24,12 @@ export function FeedbackForm() {
     <Fragment>
       <span className="block">
         <span className="block">
-          <Textarea {...feedbackTextarea.props} />
+          <Textarea {...feedbackTextarea.props} title="Enter feedback" />
         </span>
         <span className="block mt-3">
           <Button
-            label="By submitting feedback you will help make the product better"
             {...createFeedbackButton.props}
+            label="By submitting feedback you will help make the product better"
           >
             Create feedback
           </Button>

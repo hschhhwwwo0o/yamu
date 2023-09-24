8b9c3c304caf0d7ba83fae1505dfe576ffa4c093
Saveliy Andronov <ssandry.github@gmail.com>
2023-09-24 18:29:33 +0300

    Fixs h2 styles

--- app/yamu-react-app/components/text/H2/index.tsx
+++ app/yamu-react-app/components/text/H2/index.tsx
@@ -5,12 +5,14 @@ interface H2Interface {
   className?: string;
 }
 
-export function H2({ children }: H2Interface): ReactNode {
+export function H2({ children, className = "" }: H2Interface): ReactNode {
   return (
     <Fragment>
-      <h2 className="text-2xl lg:text-3xl font-bold text-black dark:text-white mt-2">
-        {children}
-      </h2>
+      <span className={`block ${className}`}>
+        <h2 className="text-2xl lg:text-3xl font-bold text-black dark:text-white mt-2">
+          {children}
+        </h2>
+      </span>
     </Fragment>
   );
 }

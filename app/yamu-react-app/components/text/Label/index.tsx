3c922e628c450ffbd993e5592b8b6ccc0981d2d4
Saveliy Andronov <ssandry.github@gmail.com>
2023-09-26 16:34:01 +0300

    Labels fixs

--- app/yamu-react-app/components/text/Label/index.tsx
+++ app/yamu-react-app/components/text/Label/index.tsx
@@ -2,13 +2,24 @@ import React, { Fragment, ReactNode } from "react";
 
 interface LabelInterface {
   children: ReactNode;
+  className?: string;
+  onClick?: () => void;
 }
 
-export function Label({ children }: LabelInterface): ReactNode {
+export function Label({
+  children,
+  className = "",
+  onClick = () => undefined,
+}: LabelInterface): ReactNode {
   return (
     <Fragment>
-      <span className="block text-sm cursor-default text-[#181818] opacity-60 dark:text-white dark:opacity-60">
-        {children}
+      <span
+        onClick={onClick}
+        className={`block text-[#181818] dark:text-white cursor-default ${className}`}
+      >
+        <span className="block text-sm text-[#181818] opacity-60 dark:text-white dark:opacity-60">
+          {children}
+        </span>
       </span>
     </Fragment>
   );

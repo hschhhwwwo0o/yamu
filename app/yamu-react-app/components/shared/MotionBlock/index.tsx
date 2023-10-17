eb52724769434db16c1c934795a24a247cdf48e4
Saveliy Andronov <ssandry.github@gmail.com>
2023-10-17 19:25:31 +0300

    Fix animations

--- app/yamu-react-app/components/shared/MotionBlock/index.tsx
+++ app/yamu-react-app/components/shared/MotionBlock/index.tsx
@@ -7,25 +7,44 @@ import { motion } from "framer-motion";
 interface MotionBlockInterfaceProps {
   delay?: number;
   children?: ReactNode;
+  isWhileInView?: boolean;
 }
 
 export function MotionBlock({
   delay = 0,
   children,
+  isWhileInView = false,
 }: MotionBlockInterfaceProps): React.JSX.Element {
   return (
     <Fragment>
-      <motion.span
-        initial={{ opacity: 0.2 }}
-        whileInView={{ opacity: 1 }}
-        transition={{
-          duration: 0.9,
-          delay,
-        }}
-        className="block"
-      >
-        {children}
-      </motion.span>
+      {isWhileInView === true && (
+        <motion.span
+          initial={{ opacity: 0.2 }}
+          whileInView={{ opacity: 1 }}
+          transition={{
+            duration: 0.9,
+            delay,
+          }}
+          className="block"
+        >
+          {children}
+        </motion.span>
+      )}
+      {isWhileInView === false && (
+        <Fragment>
+          <motion.span
+            initial={{ opacity: 0.2 }}
+            animate={{ opacity: 1 }}
+            transition={{
+              duration: 0.9,
+              delay,
+            }}
+            className="block"
+          >
+            {children}
+          </motion.span>
+        </Fragment>
+      )}
     </Fragment>
   );
 }

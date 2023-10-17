ef92c0c03316ee7544f189cad977c195a4350e37
Saveliy Andronov <ssandry.github@gmail.com>
2023-10-17 19:15:42 +0300

    Fix zoom on ios

--- app/yamu-react-app/app/layout.tsx
+++ app/yamu-react-app/app/layout.tsx
@@ -26,6 +26,12 @@ export default function RootLayout({
 }): React.JSX.Element {
   return (
     <html lang="en" className="bg-[#FDFDFD] dark:bg-black">
+      <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
+      </head>
       <body className={inter.className}>
         <div>
           <TheTopNavigation />

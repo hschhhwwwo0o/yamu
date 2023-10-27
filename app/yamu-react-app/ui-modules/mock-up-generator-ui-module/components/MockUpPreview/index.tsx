67c0b6d38590e2f9af9b4633118c28d4c67b5c62
Saveliy Andronov <ssandry.github@gmail.com>
2023-10-27 15:15:50 +0300

    Final ui-modules

--- app/yamu-react-app/ui-modules/mock-up-generator-ui-module/components/MockUpPreview/index.tsx
+++ app/yamu-react-app/ui-modules/mock-up-generator-ui-module/components/MockUpPreview/index.tsx
@@ -4,16 +4,16 @@ import React, { Fragment } from "react";
 import { observer } from "mobx-react-lite";
 import { MockUpImageViewController } from "../../controllers/_mock-up-image-state-view-controller";
 
+/** Styles */
+import styles from "./styles.module.css";
+
 /** @requirement UF/MOCK-UP/RENDER */
 export const MockUpPreview = observer(function MockUpPreview() {
   return (
     <Fragment>
       {MockUpImageViewController.image && (
         <img
-          style={{
-            objectFit: "contain",
-          }}
-          className="h-full w-full object-contain"
+          className={styles.preview}
           src={MockUpImageViewController.image}
           alt="Mock-up"
         />

9de174a4754eabd2533b5e8b0449cccb51a54ece
Saveliy Andronov <andronov.savely@yandex.ru>
2024-02-19 12:13:02 +0300

    Connect gtag

--- app/yamu-react-app/app/layout.tsx
+++ app/yamu-react-app/app/layout.tsx
@@ -32,6 +32,14 @@ export default function RootLayout({
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
         />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-F6SK27KP9E"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-F6SK27KP9E');
+        </script>
       </head>
       <body className={inter.className}>
         <div>

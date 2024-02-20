6db433375cc07a8335951ec325b6f0a60c07269d
Saveliy Andronov <ssandry.github@gmail.com>
2024-02-20 15:38:41 +0300

    Update layout.tsx

--- app/yamu-react-app/app/layout.tsx
+++ app/yamu-react-app/app/layout.tsx
@@ -50,10 +50,8 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         </Script>
       </head>
       <body className={inter.className}>
-        <!-- Google Tag Manager (noscript) -->
 <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K8Q2KN4R"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
         <div>
           <TheTopNavigation />
           <Fragment>{children}</Fragment>
